3639a588741c8aae61db3ba20d82316f6a3eb862
BlankRain <731014656@qq.com>
2016-01-25 11:40:46 +0800

    add LnObject

---
+++ LnObject.java
@@ -0,0 +1,10 @@
+package Ln_S.RunBox;
+
+public abstract class LnObject {
+	public LnObject() {
+	}
+
+	public abstract LnObject 静();// value in String
+
+	public abstract LnObject 动();// value in method
+}
